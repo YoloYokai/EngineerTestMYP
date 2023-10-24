b9c5501a1eccea6aba14d23fc7a1e47aecd74365
Joshua Rowley <joshua.rowley@endataclaims.com>
2023-10-25 09:58:01 +1000

    [Refactor] drugbenefitupdate in prep for update

--- EngineerTest/Pharmacy.cs
+++ EngineerTest/Pharmacy.cs
@@ -3,14 +3,58 @@ namespace EngineerTest;
 public class Pharmacy : IPharmacy
 {
     private readonly List<IDrug> _drugs = new();
-
+    private int _Benefitdecrease = 1;
+    private int _ExpiredMulti = 2;
     public IDrug AddDrug(string name, int expiresIn, int benefit)
     {
         var drug = new Drug(name, expiresIn, benefit);
         _drugs.Add(drug);
         return drug;
     }
-
+    public IEnumerable<IDrug> UpdateBenefitValueRefactor()
+    {
+        foreach(var drug in _drugs)
+        {
+            if(drug.ExpiresIn <= 0)
+            {
+                switch (drug.Name)
+                {
+                    case "Herbal Tea":
+                        drug.Benefit = drug.Benefit + _Benefitdecrease;
+                        break;
+                    case "Magic Pill":
+                        break;
+                    case "Fervex":
+                        if (drug.ExpiresIn <= 5) drug.Benefit += 3;
+                        else if (drug.ExpiresIn <= 10) drug.Benefit += 2;
+                        break;
+                    default:
+                        drug.Benefit = drug.Benefit - _Benefitdecrease;
+                        break;
+                }
+            }
+            else
+            {
+                switch (drug.Name)
+                {
+                    case "Herbal Tea":
+                        drug.Benefit = drug.Benefit + _Benefitdecrease * 2;
+                        break;
+                    case "Magic Pill":
+                        break;
+                    case "Fervex":
+                        drug.Benefit = 0;
+                        break;
+                    default:
+                        drug.Benefit = drug.Benefit - (_Benefitdecrease * _ExpiredMulti);
+                        break;
+                }
+            }
+            if(drug.Benefit < 0) drug.Benefit = 0;
+            if(drug.Benefit > 50) drug.Benefit = 50;
+        }
+        return _drugs;
+    }
     public IEnumerable<IDrug> UpdateBenefitValue()
     {
         for (var i = 0; i < _drugs.Count; i++)
